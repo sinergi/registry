913c58a77fdf2d81da4607d68072e3def863321c
Gabriel Bull <me@gabrielbull.com>
2014-07-09 06:55:51 -0400

    Added handy properties to the Application class

--- src/Registry/Application.php
+++ src/Registry/Application.php
@@ -8,6 +8,26 @@ class Application
      */
     private $applications = [];
 
+    /**
+     * @var string
+     */
+    private $env;
+
+    /**
+     * @var string
+     */
+    private $runtime;
+
+    /**
+     * @var string
+     */
+    private $srcDir;
+
+    /**
+     * @var string
+     */
+    private $rootDir;
+
     public function run()
     {
         foreach ($this->applications as $application) {
@@ -29,5 +49,77 @@ class Application
     private function runApplication(ApplicationInterface $application)
     {
         $application->run();
+    }
+
+    /**
+     * @param string $env
+     * @return $this
+     */
+    public function setEnv($env)
+    {
+        $this->env = $env;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEnv()
+    {
+        return $this->env;
+    }
+
+    /**
+     * @param string $runtime
+     * @return $this
+     */
+    public function setRuntime($runtime)
+    {
+        $this->runtime = $runtime;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRuntime()
+    {
+        return $this->runtime;
+    }
+
+    /**
+     * @param string $srcDir
+     * @return $this
+     */
+    public function setSrcDir($srcDir)
+    {
+        $this->srcDir = $srcDir;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSrcDir()
+    {
+        return $this->srcDir;
+    }
+
+    /**
+     * @param string $rootDir
+     * @return $this
+     */
+    public function setRootDir($rootDir)
+    {
+        $this->rootDir = $rootDir;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRootDir()
+    {
+        return $this->rootDir;
     }
 }
